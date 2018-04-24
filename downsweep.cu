8ddf187ed924a940fb31d82b81a585ab67143931
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-24 11:55:35 -0400

    Update downsweep.cu

--- downsweep.cu
+++ downsweep.cu
@@ -82,7 +82,7 @@ __global__ void prefix_downsweepsweep_kernel (int *b_d, int *a_d, int n, int dep
          b_d[tid] = smem[threadIdx.x];  
         if (threadIdx.x ==  blockDim.x -1 && blockIdx.x != 0){
             smem[threadIdx.x] = blocksum_device[blockIdx.x-1];
-             b_d[tid] = blocksum_device[blockIdx.x];  
+             b_d[tid] = blocksum_device[blockIdx.x-1];  
         }
       /*  if (tid =1){
             printf("\n checking  blocksum_device[blockIdx.x] %d %d %d %d \n",  blocksum_device[0], blocksum_device[1],blocksum_device[2],blocksum_device[3] );
