410f411ae482af199c9a59515bb82eb712a0977f
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-24 11:52:57 -0400

    Update downsweep.cu

--- downsweep.cu
+++ downsweep.cu
@@ -108,7 +108,7 @@ __global__ void prefix_downsweepsweep_kernel (int *b_d, int *a_d, int n, int dep
                 printf("\n printing first downsweep tid  %d  %d  %d  %d", tid, tmp1, tmp3, smem[threadIdx.x]);
                 //offset /= 2;
             }
-            
+            offset /= 2;
 
         } // end for loop 
 
