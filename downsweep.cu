894c51ca922b3dd9be4771893900d337947bc230
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-24 16:16:37 -0400

    Update downsweep.cu

--- downsweep.cu
+++ downsweep.cu
@@ -25,7 +25,7 @@ using namespace std;
 
 void fillPrefixSum(int arr[], int n, int prefixSum[])
 {
-    prefixSum[0] = arr[0];
+    prefixSum[0] = 0;
     for (int i = 1; i < n; i++)
         prefixSum[i] = prefixSum[i-1] + arr[i];
 }
